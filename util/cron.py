ea699fca1931bd8f50d9f7207d3ce89646b75024
Joao Faro <projetos@joaofaro.eng.br>
2024-05-26 19:15:50 -0300

    add lotomania cron

--- util/cron.py
+++ util/cron.py
@@ -23,3 +23,11 @@ def get_cron(game_type):
         if index_week == 7:
             cron = 'cron(50 23 ? * 2 *)'
         return cron
+    if game_type == 3:
+        if index_week == 1 or index_week > 5:
+            cron = 'cron(50 23 ? * 2 *)'
+        if index_week == 2 or index_week == 3:
+            cron = 'cron(50 23 ? * 4 *)'
+        if index_week == 4 or index_week == 5:
+            cron = 'cron(50 23 ? * 6 *)'
+        return cron
