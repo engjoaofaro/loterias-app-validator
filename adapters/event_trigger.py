c94efb6aaadab21f8b35df70cb9ffd3d69871206
Joao Faro <projetos@joaofaro.eng.br>
2024-05-26 15:39:33 -0300

    refactoring crons

--- adapters/event_trigger.py
+++ adapters/event_trigger.py
@@ -1,34 +1,18 @@
-import datetime
-
 import boto3
 import os
-from datetime import datetime
-from pytz import timezone
+from util.cron import get_cron
 
 client = boto3.client('scheduler')
 
 
 def schedule(game_type):
-    cron = 'cron(00 01 * * ? *)'
-    today = datetime.now()
-    fuz = timezone('America/Sao_Paulo')
-    date_sao_paulo = today.astimezone(fuz)
-    index_week = date_sao_paulo.isoweekday()
 
     if game_type == 1:
-        if index_week <= 2:
-            cron = 'cron(50 23 ? * 3 *)'
-        elif index_week == 3 or index_week == 4:
-            cron = 'cron(50 23 ? * 5 *)'
-        elif index_week == 5 or index_week == 6:
-            cron = 'cron(50 23 ? * 7 *)'
-        elif index_week == 7:
-            cron = 'cron(50 23 ? * 3 *)'
+        cron = get_cron(1)
         update_schedule('schedule-step-function-flow', cron)
 
     if game_type == 2:
-        if index_week == 7:
-            cron = 'cron(50 23 ? * 2 *)'
+        cron = get_cron(2)
         update_schedule('schedule-step-function-flow_2', cron)
 
 
---
+++ util/cron.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from pytz import timezone
+
+
+def get_cron(game_type):
+    cron = 'cron(00 01 * * ? *)'
+    today = datetime.now()
+    fuz = timezone('America/Sao_Paulo')
+    date_sao_paulo = today.astimezone(fuz)
+    index_week = date_sao_paulo.isoweekday()
+
+    if game_type == 1:
+        if index_week <= 2:
+            cron = 'cron(50 23 ? * 3 *)'
+        elif index_week == 3 or index_week == 4:
+            cron = 'cron(50 23 ? * 5 *)'
+        elif index_week == 5 or index_week == 6:
+            cron = 'cron(50 23 ? * 7 *)'
+        elif index_week == 7:
+            cron = 'cron(50 23 ? * 3 *)'
+        return cron
+    if game_type == 2:
+        if index_week == 7:
+            cron = 'cron(50 23 ? * 2 *)'
+        return cron
